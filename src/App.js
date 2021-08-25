08c698a81f0259534d32371be860c7a0677eb4f6
Wilson Pinto <wilson-pinto>
2021-08-25 21:34:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-primary text-light p-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, atque. Itaque veritatis autem perspiciatis ipsum saepe quo? Nemo nobis iure rem architecto tempore perferendis, molestiae ratione ipsa animi vel. Ad.
+      </div>
+      <div className="bg-dark text-light p-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, atque. Itaque veritatis autem perspiciatis ipsum saepe quo? Nemo nobis iure rem architecto tempore perferendis, molestiae ratione ipsa animi vel. Ad.
+      </div>
+      <div className="bg-warning text-light p-5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, atque. Itaque veritatis autem perspiciatis ipsum saepe quo? Nemo nobis iure rem architecto tempore perferendis, molestiae ratione ipsa animi vel. Ad.
+      </div>
+    </>
+  )
+}
+
+export default App
+
